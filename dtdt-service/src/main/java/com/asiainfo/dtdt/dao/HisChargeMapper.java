b663e08d70666aa249bbdec98e1d0a60a6f2512f
刘延森 <liuys@asiainfo.com>
2017-06-29 17:59:15 +0800

    移动mapper路径

--- dtdt-service/src/main/java/com/asiainfo/dtdt/dao/HisChargeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.asiainfo.dtdt.dao;
-
-import com.asiainfo.dtdt.entity.HisCharge;
-
-public interface HisChargeMapper {
-    int insert(HisCharge record);
-
-    int insertSelective(HisCharge record);
-}
--- dtdt-service/src/main/java/com/asiainfo/dtdt/dao/ParamMapper.java
+++
@@ -1,9 +0,0 @@
-package com.asiainfo.dtdt.dao;
-
-import com.asiainfo.dtdt.entity.Param;
-
-public interface ParamMapper {
-    int insert(Param record);
-
-    int insertSelective(Param record);
-}
