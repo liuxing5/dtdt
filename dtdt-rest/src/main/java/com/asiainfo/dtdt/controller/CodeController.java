94b87cb4ead188c572ce5911eb77f4f56f02ffa8
liuxing5 <liuxing5@asiainfo.com>
2017-06-30 19:33:03 +0800

    提交未使用：CodeController,TokenController  //TODO

--- dtdt-rest/src/main/java/com/asiainfo/dtdt/controller/CodeController.java
+++ dtdt-rest/src/main/java/com/asiainfo/dtdt/controller/CodeController.java
@@ -1,33 +1,33 @@
-package com.asiainfo.dtdt.controller;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.asiainfo.dtdt.interfaces.ICodeService;
-/**
- * @author liuixng5
- */
-@Controller
-@RequestMapping(value="/code", method=RequestMethod.POST)
-public class CodeController {
-	
-	private static final Logger logger = Logger.getLogger(CodeController.class);
-	
-	@Autowired
-	private ICodeService codeService;
-	
-	@RequestMapping(value="/getCode", produces = "application/json; charset=utf-8")
-	@ResponseBody
-	public String getToken(HttpServletRequest request, String phone) {
-		logger.info("CodeController getCode() phone=" + phone);
-		String partnerCode = request.getParameter("partnerCode");
-		String appKey = request.getParameter("appKey");
-		return codeService.getCode(partnerCode, appKey, phone);
-	}
-}
+//package com.asiainfo.dtdt.controller;
+//
+//import javax.servlet.http.HttpServletRequest;
+//
+//import org.apache.log4j.Logger;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import com.asiainfo.dtdt.interfaces.ICodeService;
+///**
+// * @author liuixng5
+// */
+//@Controller
+//@RequestMapping(value="/code", method=RequestMethod.POST)
+//public class CodeController {
+//	
+//	private static final Logger logger = Logger.getLogger(CodeController.class);
+//	
+//	@Autowired
+//	private ICodeService codeService;
+//	
+//	@RequestMapping(value="/getCode", produces = "application/json; charset=utf-8")
+//	@ResponseBody
+//	public String getToken(HttpServletRequest request, String phone) {
+//		logger.info("CodeController getCode() phone=" + phone);
+//		String partnerCode = request.getParameter("partnerCode");
+//		String appKey = request.getParameter("appKey");
+//		return codeService.getCode(partnerCode, appKey, phone);
+//	}
+//}
--- dtdt-rest/src/main/java/com/asiainfo/dtdt/controller/TokenController.java
+++ dtdt-rest/src/main/java/com/asiainfo/dtdt/controller/TokenController.java
@@ -1,29 +1,29 @@
-package com.asiainfo.dtdt.controller;
-
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.asiainfo.dtdt.interfaces.ITokenService;
-/**
- * @author liuixng5
- */
-@Controller
-@RequestMapping(value="/token", method=RequestMethod.POST)
-public class TokenController {
-	
-	private static final Logger logger = Logger.getLogger(TokenController.class);
-	
-	@Autowired
-	private ITokenService tokenService;
-	
-	@RequestMapping(value="/getToken", produces = "application/json; charset=utf-8")
-	@ResponseBody
-	public String getToken(String partnerCode, String appKey) {
-		logger.info("TokenController getToken() partnerCode=" + partnerCode + " appKey=" + appKey);
-		return tokenService.getToken(partnerCode, appKey);
-	}
-}
+//package com.asiainfo.dtdt.controller;
+//
+//import org.apache.log4j.Logger;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import com.asiainfo.dtdt.interfaces.ITokenService;
+///**
+// * @author liuixng5
+// */
+//@Controller
+//@RequestMapping(value="/token", method=RequestMethod.POST)
+//public class TokenController {
+//	
+//	private static final Logger logger = Logger.getLogger(TokenController.class);
+//	
+//	@Autowired
+//	private ITokenService tokenService;
+//	
+//	@RequestMapping(value="/getToken", produces = "application/json; charset=utf-8")
+//	@ResponseBody
+//	public String getToken(String partnerCode, String appKey) {
+//		logger.info("TokenController getToken() partnerCode=" + partnerCode + " appKey=" + appKey);
+//		return tokenService.getToken(partnerCode, appKey);
+//	}
+//}
